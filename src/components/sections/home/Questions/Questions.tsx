8f207e8ec3a0222197666aff1f2746ad96c6890c
kubolion <momot.vova@inbox.ru>
2025-04-02 05:39:43 +0300

    one

---
+++ src/components/sections/home/Questions/Questions.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+import Image from "next/image"
+import styles from "./Ansver.module.scss"
+
+
+const Ansver = () => {
+    return (
+
+        <section className={`${styles.container} container`} >
+
+    )
+}
+
+export default Ansver
